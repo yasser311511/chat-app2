a6612a768a77f3a6bc6d547f67c1d6489ce74491
yasser311511 <jalilfg435@gmail.com>
2025-08-30 12:38:35 +0100

    fix: use process.env.PORT for Render

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
